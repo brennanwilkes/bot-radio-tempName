bd87ee781a22204bf25e25156d5b6efd2dbd2e56
Brennan Wilkes <brennan@codexwilkes.com>
2020-10-13 15:59:15 -0700

    feat(discordBot): Plays first random song of playlist

--- src/discordBot.py
+++ src/discordBot.py
@@ -2,7 +2,8 @@ import discord
 from discord.ext import commands
 import requests
 import shutil
-import os, sys
+import os, sys, random
+import glob
 
 #may need to fix later
 import spotifyConnection as spot
@@ -12,62 +13,68 @@ PREFIX_PATH = sys.path[0]
 
 
 def token():
-    TOKEN_FILE = PREFIX_PATH+"/auth/discordToken"
-    fname = open(TOKEN_FILE)
-    token = fname.read()
-    return token
+	TOKEN_FILE = PREFIX_PATH+"/auth/discordToken"
+	fname = open(TOKEN_FILE)
+	token = fname.read()
+	return token
 
 
 class MyClient(discord.Client):
-    spotC = ""
-    playlist = ""
-    commandChar = "."
+	spotC = None
+	playlist = None
+	commandChar = "."
 
-    def makePlaylist(self, url):
-        self.spotC = spot.spotifyConnection()
-        self.playlist = playlist.playlist(self.spotC.loadPlaylist(url))
+	def makePlaylist(self, url):
+		self.playlist = playlist.playlist(self.spotC.loadPlaylist(url))
+		random.shuffle(self.playlist.songs)
 
-    '''
-    #not implemented yet
-    async def leave(self):
-        await self.voice_client.disconnect()
-    '''
+	'''
+	#not implemented yet
+	async def leave(self):
+		await self.voice_client.disconnect()
+	'''
 
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+	async def on_ready(self):
+		self.spotC = spot.spotifyConnection()
+		print('Logged on as {0}!'.format(self.user))
 
-    async def on_message(self, message):
-        if (not len(message.content)) or (not message.content[0] == self.commandChar):
-            return
-        
-        args = message.content.split(" ")
+	async def getSongSource(self,fn):
+		if os.name == 'nt': #windows
+			source = await discord.FFmpegOpusAudio.from_probe(executable="C:/Program Files/ffmpeg/bin/ffmpeg.exe", source = fn, method='fallback')
+		else:
+			source = await discord.FFmpegOpusAudio.from_probe(executable="ffmpeg", source = fn, method='fallback')
+		return source
 
-        if args[0] == ".queue":
-            if not playlist:
-                await message.channel.send("Error! Playlist is empty")
-            else:
-                await message.channel.send('\n'.join([s.name for s in self.playlist.songs]))
-        elif args[0] == ".play":
-            self.makePlaylist(args[1])
-        
-            #connect to the voice channel that the person who wrote the message is in
-            VC = await message.author.voice.channel.connect()
-            '''
-            #From music bot discord.py example - might need this in the future
-            if ctx.voice_client is not None:
-                return await ctx.voice_client.move_to(channel)
-            '''
+	async def on_message(self, message):
+		if (not len(message.content)) or (not message.content[0] == self.commandChar):
+			return
 
-            song_path = PREFIX_PATH + "/test.webm"
+		args = message.content.split(" ")
 
-            if os.name == 'nt': #windows
-                source = await discord.FFmpegOpusAudio.from_probe(executable="C:/Program Files/ffmpeg/bin/ffmpeg.exe", source = song_path, method='fallback')
-            else:
-                source = await discord.FFmpegOpusAudio.from_probe(executable="ffmpeg", source = song_path, method='fallback')
-            VC.play(source)
+		if args[0] == ".queue":
+			if not playlist:
+				await message.channel.send("Error! Playlist is empty")
+			else:
+				await message.channel.send('\n'.join([s.name for s in self.playlist.songs]))
+		elif args[0] == ".play":
+			self.makePlaylist(args[1])
+
+			self.playlist.songs[0].downloadAudio(override=True,debug=True)
+
+			#connect to the voice channel that the person who wrote the message is in
+			VC = await message.author.voice.channel.connect()
+			'''
+			#From music bot discord.py example - might need this in the future
+			if ctx.voice_client is not None:
+				return await ctx.voice_client.move_to(channel)
+			'''
+
+			print("Playing",self.playlist.songs[0].name)
+
+			VC.play(await self.getSongSource(glob.glob(PREFIX_PATH+"/../audioCache/"+self.playlist.songs[0].youtubeID+".*")[0]))
 
 
-        print('Message from {0.author}: {0.content}'.format(message))
-        secret_passphrase = "wah"
-        if message.content == secret_passphrase:# and message.author.id == 224020595103236096:
-            await message.channel.send("no u")
+		print('Message from {0.author}: {0.content}'.format(message))
+		secret_passphrase = "wah"
+		if message.content == secret_passphrase:# and message.author.id == 224020595103236096:
+			await message.channel.send("no u")
